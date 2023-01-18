0a6d40bae88403931819e716793e03645f9bf7fe
Deverly Gousse <deverlygousse@gmail.com>
2023-01-18 15:21:59 -0500

    first commit

---
+++ src/test/java/appium_java_testing/Tests.java
@@ -0,0 +1,45 @@
+package appium_java_testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+
+public class Tests {
+	
+	//AndroidDriver driver;
+	AppiumDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("platformVersion", "11");
+		capabilities.setCapability("appPackage", "com.example.new_sample");
+		capabilities.setCapability("appActivity", "com.example.new_sample.MainActivity");
+		driver = new AppiumDriver(capabilities);
+		
+	}
+	
+	@Test
+	public void test() {
+		System.out.println("Application started ...");
+		driver.findElement(By.id("button2")).click();
+		driver.findElement(By.id("button_multi")).click();
+		driver.findElement(By.id("button2")).click();
+		Assert.assertEquals("4", driver.findElement(By.id("output")).getText());
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		
+		driver.close();
+		
+	}
+
+}
