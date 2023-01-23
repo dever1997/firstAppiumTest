b63683a61a20b49f28ef33dd3eac9577ec3f8323
dever1997 <72216293+dever1997@users.noreply.github.com>
2023-01-23 08:56:45 -0500

    Update Tests.java

--- src/test/java/appium_java_testing/Tests.java
+++ src/test/java/appium_java_testing/Tests.java
@@ -7,6 +7,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.URL;
+
 import io.appium.java_client.AppiumDriver;
 
 public class Tests {
